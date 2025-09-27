c95ace388b37b0e7c3c25d913e22d707fccdc9ac
Lara Klopper <larak@corprecover.co.za>
2025-09-27 13:50:43 +0200

    appSetup

--- server/app.js
+++ server/app.js
@@ -3,6 +3,36 @@ const ensureJwtSecret = require('./util/ensureJwtKey')
 ensureJwtSecret();
 
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+const connectDB = require('./config/connect')
+
+const port = process.env.PORT || 3001;
+
+if (!port) {
+    console.error('[ERROR: app.js] port enviromental variable is missing');
+    process.exit(1)
+}
 
 const app = express()
 
+//===========SETUP MIDDLEWARE======================
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+//================MOUNT ROUTES========
+mongoose.set('strictPopulate', false);
+
+//==========START THE SERVER================
+// Start the server and listen on the specified port after the DB connects
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.info(`[INFO: app.js] Server is running on port: ${port}`);
+    })
+}).catch(error => {
+    console.error('[ERROR: app.js] Database connection failed:', error);
+    process.exit(1);
+})
