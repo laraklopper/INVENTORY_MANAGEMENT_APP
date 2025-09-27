abf3b74129a355fea14dd1a89188bcc2fb618743
Lara Klopper <larak@corprecover.co.za>
2025-09-27 14:24:05 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default function App() {
+  const [error, setError] =useState(null)
+
   return (
-    <div>App</div>
+    <>
+    <Container>
+        <Row>
+          <Col></Col>
+          <Col xs={5}>
+            {/* Error message */}
+            {error && <p id='errorMessage'>{error}</p>}
+          </Col>
+          <Col></Col>
+        </Row>
+        <Routes>
+          
+        </Routes>
+
+    </Container>
+    
+    </>
   )
 }
