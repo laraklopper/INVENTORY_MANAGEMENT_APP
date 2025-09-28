58954e2b79c172589ba4a1b3f99363828d0d22de
Lara Klopper <larak@corprecover.co.za>
2025-09-28 12:58:14 +0200

    changes

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+// import Button from 'react-bootstrap/Button';
 
 export default function RegistrationForm() {
+    const [viewPassword, setViewPassword] = useState(false)
   return (
-    <form>
-        
+    <form id='registrationForm'>
+          <Row>
+              <Col xs={6} md={4}>
+                  <label className='regisLabel'>
+                    <p className='labelText'>USERNAME:</p>
+                    <input
+                    className='input'
+                    type='text'
+                    />
+                  </label>
+              </Col>
+              <Col xs={6} md={4}>
+                  <label className='regisLabel'>
+                      <p className='labelText'>COMPANY NAME:</p>
+                      <input 
+                      className='input'
+                      type='text'
+                      />
+                  </label>
+              </Col>
+              <Col xs={6} md={4}>
+                  <label className="regisLabel" htmlFor="positionSelect">
+                      <p className='labelText'>POSITION:</p>
+                      <select
+                          className="input"
+                          id="positionSelect"
+                          name="position"
+                          required
+                          aria-label="Select position"
+                          aria-required="true"
+                      >
+                          <option className="option" value="">SELECT</option>
+                          <option className="option" value="manager">MANAGER</option>
+                          <option className="option" value="admin">ADMIN</option>
+                          <option className="option" value="clerk">CLERK</option>
+                          <option className="option" value="viewer">VIEWER</option>
+                      </select>
+                  </label>
+              </Col>
+          </Row>
+          <Row>
+              <Col>
+                  <Stack gap={3}>
+                      <div className="p-2">First item</div>
+                  </Stack>
+              </Col>
+              
+          </Row>
+          <Row>
+              <Col>
+                  <Stack gap={3}>
+                      <div className="p-2">First item</div>
+                  </Stack>
+              </Col>
+          </Row>
+          <Row>
+              <Col xs={12} md={8}>
+                  xs=12 md=8
+              </Col>
+              <Col xs={6} md={4}>
+                  xs=6 md=4
+              </Col>
+          </Row>
     </form>
   )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import MainHeader from '../components/MainHeader';
+
+
 export default function Register() {
     
   return (
     <>
     <Container>
         <MainHeader mainHeading='REGISTER'/>
+        <section>
+     
+
+        </section>
     </Container>
     </>
   )
