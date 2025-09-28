0f5274ffa70ed39109f27ceb90bb7f966ef9945b
Lara Klopper <larak@corprecover.co.za>
2025-09-28 10:45:57 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,27 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 export default function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [users, setUser] = useState([])
+  const [userData, setUserData] = useState({
+    username: '',
+    companyName: '',
+    position: '',
+    fullName: {
+      firstName: '',
+      lastName: '',
+    },
+    contactDetails: {
+      email: '',
+      contactNumber: '',
+    },
+    dateOfBirth: '',
+    password: '',
+  })
+  const [loggedIn, setLoggedIn] = useState(false)
   const [error, setError] =useState(null)
 
+  //==========================================
   return (
     <>
     <Container>
