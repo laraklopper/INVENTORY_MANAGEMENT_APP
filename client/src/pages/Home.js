504e4cf9ba210faf54bbb3398c5abb82e0ac1541
Lara Klopper <larak@corprecover.co.za>
2025-09-29 12:57:45 +0200

    changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import MainHeader from '../components/MainHeader';
+import Card from 'react-bootstrap/Card';
 
 export default function Home() {
   return (
     <>
-    <Container>
-        <MainHeader mainHeading='HOME'/>
-    </Container>
+  
+        <Card id='homePageCard'> 
+          <Card.Body>
+            <MainHeader mainHeading='HOME' />
+          </Card.Body>
+        </Card>
+
+        
+
     </>
   )
 }
