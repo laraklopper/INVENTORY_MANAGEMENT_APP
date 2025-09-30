183d150dc0a6e5a5e339f07e6617f8919bec23b1
Lara Klopper <larak@corprecover.co.za>
2025-09-30 08:20:29 +0200

    functions

---
+++ client/src/functions/dateFunctions.js
@@ -0,0 +1,27 @@
+export const dateDisplay = (dateString) => {
+    const options = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    }
+    return new Date(dateString).toLocaleDateString('en-GB', options);
+}
+
+// Format time as hh:mm:ss
+export const timeDisplay = (dateObj) => {
+    return dateObj.toLocaleTimeString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    });
+};
+
+export const formatDate = (value) =>
+    value
+        ? new Date(value).toLocaleDateString
+            ('en-ZA', {
+                year: 'numeric',
+                month: 'short',
+                day: '2-digit'
+            }) : '—';
