30208e1b56337c92c73c5fa67231bd3e5ed38183
Lara Klopper <larak@corprecover.co.za>
2025-10-01 16:35:54 +0200

    calculator

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './pageCss/Account.css'
 import '../CSS/DataList.css'
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-// import Button from 'react-bootstrap/Button';
-// import Stack from 'react-bootstrap/Stack';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
@@ -17,8 +17,66 @@ export default function Account({currentUser ,logout}) {
     <>
     <Header currentUser={currentUser}/>
    
-    <section id='accountSection2'>
-        
+    <section id='accountSection1'>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={5}>
+            <Stack gap={3}>
+              <div className="p-2">
+                <h2 id='calculatorHeading'>INTEREST CALCULATOR</h2>
+              </div>
+              <div className="p-2">
+                <label>
+                  <p className='labelText'>AMOUNT:</p>
+                  <p></p>
+                  <input
+                    id='amountInput'
+                    className='input'
+                    type='number'
+                  />
+                </label>
+              </div>
+              <div className="p-2">                <label>
+                <p className='labelText'>INTEREST:</p>
+                
+                <input
+                  id='amountInput'
+                  className='input'
+                  type='number'
+                />
+              </label></div>
+              <div className="p-2">
+                <p className='labelText'>TIME:</p>
+                {/* Time Period Input */}
+
+                <input
+                  id='amountInput'
+                  className='input'
+                  type='number'
+                />
+              </div>
+              <div className="p-2">
+                <Button>
+                  CALCULATE INTEREST
+                </Button>
+              </div>
+            </Stack>
+            <Stack gap={3}>
+              <div className="p-2"></div>
+              <div className="p-2">
+                <span>
+                  <h6 className='interest'>INTEREST</h6>
+                  <h6 className='interest'><strong>R</strong></h6>
+                  <h6 className='interest'>
+                    {/* totalInterest */}
+                  </h6>
+                </span>
+              </div>
+              <div className="p-2"></div>
+            </Stack>
+          </Col>
+          <Col></Col>
+        </Row>
     </section>
     <Footer logout={logout} currentUser={currentUser}/>
     </>
