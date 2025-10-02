14ddb36b650e68498dda852ebc16d9f5e783b535
Lara Klopper <larak@corprecover.co.za>
2025-10-02 10:49:00 +0200

    canges

--- client/src/components/InterestCalculator.js
+++ client/src/components/InterestCalculator.js
@@ -1,8 +1,122 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../CSS/Forms.css'
+// Bootstrap
+import Button from 'react-bootstrap/Button'; // Import Bootstrap Button component
+import Row from 'react-bootstrap/Row';       // Import Bootstrap Row layout
+import Col from 'react-bootstrap/Col';       // Import Bootstrap Column layout
+import Stack from 'react-bootstrap/Stack';
+
 
 export default function InterestCalculator() {
+    const [principal, setPrincipal] = useState(0)
+    const [interestRate, setInterestRate] = useState(0); // State  used to store the interest rate
+    const [timePeriod, setTimePeriod] = useState(0); // State used to store the time period
+    const [totalInterest, setTotalInterest] = useState(0); // Stores the calculated total interest
+
+    //=========EVENT LISTENERS==================
+    // Event handler for principal input change
+    function handlePrincipalChange(event) {
+        setPrincipal(event.target.value);
+    }
+
+    // Event handler for interest rate input change
+    function handleInterestRateChange(event) {
+        setInterestRate(event.target.value);
+    }
+
+    // Event handler for time period input change
+    function handleTimePeriodChange(event) {
+        setTimePeriod(event.target.value);
+    }
+
+    // Function to calculate the interest
+    function calculateInterest() {
+        const interest = (principal * interestRate * timePeriod) / 100; // Calculate the interest
+        setTotalInterest(interest); // Update the totalInterest state with the calculated interest
+    }
+
   return (
-    <div>InterestCalculator</div>
+    <div id='calculator'>
+          <Stack  id='calculatorDisplayStack'>
+              <div >
+                <h2 id='calculatorHeading'>INTEREST CALCULATOR</h2></div>
+              <div id='amountBlock'>
+                  <label className='calculatorLabel' htmlFor='amountInput'>
+                      <p className='labelText'>AMOUNT:</p>
+                      <div id='amountDiv'>
+                          <p className='para2'>R:</p>
+                          {/* Principal Amount input field */}
+                          <input
+                              id='amountInput'
+                              type='number'
+                              className='input'
+                              autoComplete='off'
+                              placeholder='0'
+                              value={principal}
+                              onChange={handlePrincipalChange}
+                              step='0.01'
+                              aria-label='principal amount input'
+                          />
+                      </div>
+                  
+                  </label>
+              </div>
+              <div id='interestBlock'>
+                  <label className='calculatorLabel'>
+                    <p className='labelText'>INTEREST:</p>
+                    <input
+                          type='number'
+                          id='interestRateInput'
+                          className='input'
+                          autoComplete='off'
+                          placeholder='0'
+                          value={interestRate}
+                          onChange={handleInterestRateChange}
+                          step='0.01'
+                          aria-label='Interest Rate Input'
+                    />
+                </label>
+              </div>
+              <div className="p-2" id='timeBlock'>
+                <label className='calculatorLabel'>
+                    <p className='labelText'>TIME:</p>
+                    <div id='timeDiv'>
+                          <input
+                              type='number'
+                              autoComplete='off'
+                              className='input'
+                              id='timeInput'
+                              onChange={handleTimePeriodChange}
+                              value={timePeriod}
+                              step='0.01'
+                              aria-label='Time Input'
+
+                          />
+                          <p className='labelText'>Months</p>
+                    </div>
+                    
+                </label>
+              </div>
+              <div className="p-2">
+                <Button
+                id='calculateBtn'
+                onClick={calculateInterest}
+                >
+                    CALCULATE INTEREST
+                </Button>
+              </div>
+          </Stack>
+          <Stack gap={3}>
+              <div className="p-2"></div>
+              {/* Output display for calculated interest in Rand*/}
+              <div id='interestOutput' >
+                  <div id='totalInterest'>
+                      <h6 className='interest'>INTEREST:</h6>
+                      <h6 className='interest'><strong>R</strong></h6>
+                      <h6 className='interest'>{totalInterest}</h6>
+                  </div>
+              </div>
+          </Stack>
+    </div>
   )
 }
--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -7,33 +7,16 @@ import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-
+import InterestCalculator from '../components/InterestCalculator';
+import {  Calculator } from 'lucide-react';
 
 
 export default function Account({currentUser ,logout}) {
-  const [principal, setPrincipal] = useState(0)
-  const [interestRate, setInterestRate] = useState(0)
-  const [timePeriod, setTimePeriod] = useState(0)
-  const [totalInterest, setTotalInsterest] = useState(0)
+
   const [openCalculator, setOpenCalculator] = useState(false)
 
   //===============EVENT LISTENERS=====================
-  function handlePrincipalInterest(e) {
-    setPrincipal(e.target.value)
-  }
-
-  function handleInterestRateChange(e) {
-    setInterestRate(e.target.value)
-  }
-
-  function handleTimePeriod(e) {
-    setTimePeriod(e.target.value)
-  }
-
-  function calculateInterest() {
-    const interest = (principal * interestRate * timePeriod)
-    setTotalInsterest(interest)
-  }
+ 
 
   const toggleCalculator = () => {
     setOpenCalculator(prev => !prev)
@@ -45,96 +28,32 @@ export default function Account({currentUser ,logout}) {
     <Header currentUser={currentUser}/>
    
     <section id='accountSection1'>
-        <Row>
-          <Col></Col>
-          <Col xs={5}>
-          
-          </Col>
-          <Col></Col>
-        </Row>
         <Row id='calculatorRow'>
-          <Col></Col>
-          <Col xs={5} id='calculatorCol'>
-         
-          <div>
-              <Stack gap={3}>
-                <div className="p-2">
-                  <h2 id='calculatorHeading'>INTEREST CALCULATOR</h2>
-                </div>
-                <div className="p-2">
-                  <Button
-                    variant="primary"
-                    type='button'
-                    onClick={toggleCalculator}
-                    aria-label='Toggle calculator display button'
-                    aria-pressed={openCalculator}
-
-                  >
-                    {openCalculator ? 'CLOSE CALCULATOR' : 'OPEN INTEREST CALCULATOR'}
-                  </Button>
-                </div>
-                <div className="p-2">Second item</div>
-                <div className="p-2">Third item</div>
-              </Stack>
+            <Col xs={12} md={8}>
+              
+            </Col>
+            <Col xs={6} md={4} id='calculatorCol'>
+            <div id='calculatorDiv'>
+            <Stack  id='toggleCalculatorStack' >
+              <Button
+                variant="primary"
+                type='button'
+                onClick={toggleCalculator}
+                aria-label='Toggle calculator display button'
+                aria-pressed={openCalculator}
+                id='toggleCalculatorBtn'
+              >
+                 <Calculator />
+                {openCalculator ? 'CLOSE CALCULATOR' : 'OPEN INTEREST CALCULATOR'}
+              </Button>
+            </Stack>
              
-              <Stack gap={3}>
-               
-                <div className="p-2">
-
-                  <label>
-                    <p className='labelText'>AMOUNT:</p>
-                    <p></p>
-                    <input
-                      id='amountInput'
-                      className='input'
-                      type='number'
-                    />
-                  </label>
-                </div>
-                <div className="p-2">                
-                  <label>
-                  <p className='labelText'>INTEREST:</p>
-
-                  <input
-                    id='amountInput'
-                    className='input'
-                    type='number'
-                  />
-                </label>
-                </div>
-                <div className="p-2">
-                  <p className='labelText'>TIME:</p>
-                  {/* Time Period Input */}
-
-                  <input
-                    id='amountInput'
-                    className='input'
-                    type='number'
-                  />
-                </div>
-                <div className="p-2">
-                  <Button>
-                    CALCULATE INTEREST
-                  </Button>
-                </div>
-              </Stack>
-              <Stack gap={3}>
-                <div className="p-2"></div>
-                <div className="p-2">
-                  <span>
-                    <h6 className='interest'>INTEREST</h6>
-                    <h6 className='interest'><strong>R</strong></h6>
-                    <h6 className='interest'>
-                      {/* totalInterest */}
-                    </h6>
-                  </span>
-                </div>
-                <div className="p-2"></div>
-              </Stack>
-          </div>       
-          </Col>
-          <Col></Col>
-        </Row>
+              
+                {openCalculator && <InterestCalculator/>}
+              </div>  
+            </Col>
+          </Row>
+         
     </section>
     <Footer logout={logout} currentUser={currentUser}/>
     </>
