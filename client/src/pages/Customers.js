096175c99561fece7b4103dccdacafb14982ffe8
Lara Klopper <larak@corprecover.co.za>
2025-10-03 08:32:49 +0200

    changes

--- client/src/pages/Customers.js
+++ client/src/pages/Customers.js
@@ -1,19 +1,30 @@
 import React from 'react'
-import './pageCss/Customers.css'
+import './pageCss/Customers.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import FormHeadings from '../components/FormHeadings'
 import AddCustomerForm from '../components/AddCustomerForm';
 
+
 export default function Customers({currentUser, logout}) {
   return (
     <>
     <Header heading='CUSTOMERS' currentUser={currentUser}/> 
-    <section>
-      <div>
-        <p>CLICK HERE TO</p><Button>ADD CUSTOMER</Button>
-      </div>
+    <section id='customersSection'>
+      <h2>Customers</h2>
+    </section>
+    <section id='addCustomerSection'>
+        <Row>
+          <Col></Col>
+          <Col xs={5} id='addCustomerCol'>
+            <h6 className='btnText'>CLICK HERE TO:</h6>
+            <Button>ADD CUSTOMER</Button>
+          </Col>
+          <Col></Col>
+        </Row>
       <div>
         <FormHeadings/>
         <AddCustomerForm/>
